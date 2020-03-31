2d1b30018bdb4b79177a521d8d635c0b090a2cac
Jeff Kennedy <jkennedy@usgs.gov>
2020-03-31 16:54:03 -0700

    update fg5plot

--- sgp-utils/fg5_plot.py
+++ sgp-utils/fg5_plot.py
@@ -44,6 +44,45 @@ def func(x, pos):
     s = '{:0,d}'.format(int(x))
     return s
 
+
+def make_plot(ax):
+    ax.errorbar(data[figidx + ii][0], data[figidx + ii][1], yerr=ERROR_BAR, fmt='kd')
+    # ax = plt.gca()
+    # Remove scientific notation from axes labels
+    # ax.yaxis.get_major_formatter().set_useOffset(False)
+    # Add commas to y-axis tick mark labels
+    # ax.yaxis.set_major_formatter(y_format)
+    # Set x-axis tick labels to just show year
+
+    # Adjust ticks so they fall on Jan 1 and extend past the range of the data. If there
+    # are data in January and December, add another year so that there is plenty of space.
+    if SET_XLIM:
+        ax.set_xlim([XLEFT, XRIGHT])
+        # ax.xaxis.set_major_formatter(ALTFMT)
+    else:
+        ax.xaxis.set_major_formatter(myFmt)
+        start_month = data[figidx + ii][0][0].month
+        start_year = data[figidx + ii][0][0].year
+        end_month = data[figidx + ii][0][-1].month
+        end_year = data[figidx + ii][0][-1].year
+        if start_month == 1:
+            start_year = start_year - 1
+        if end_month == 12:
+            end_year = end_year + 1
+        xticks = []
+        for iii in range(start_year, end_year + 2):
+            xticks.append(datetime.datetime(iii, 1, 1))
+        ax.set_xticks(xticks)
+    ax.set_title(stations[figidx + ii])
+    if YAXIS_LIMITS_TIGHT == 'loose':
+        max_abs_lim = max(abs(v) for v in ax.get_ylim())
+        rounded_abs_lim = math.ceil(max_abs_lim)
+        ax.set_ylim([-1 * rounded_abs_lim, rounded_abs_lim])
+    if YAXIS_FT_OF_WATER:
+        ax.set_ylabel('Storage change,\nin feet of water')
+    else:
+        ax.set_ylabel('Gravity change,\n in microGal')
+
 # Value to subtract from observed gravity so the plotted values are reasonable
 offset = 978990000
 
@@ -52,109 +91,76 @@ offset = 978990000
 data_file = filedialog.askopenfilename(title="Select text file to plot (from A10_parse.py)")
 # data_file = "SanPedro_qaqc.txt"
 
-plt.ion()
-stations = []
-myFmt = mdates.DateFormatter('%yy')
-y_format = tkr.FuncFormatter(func)
+if __name__ == "__main__":
+    plt.ion()
+    stations = []
+    myFmt = mdates.DateFormatter('%yy')
+    y_format = tkr.FuncFormatter(func)
 
-# Get station list and column numbers
-with open(data_file) as fp:
-    a = fp.readline()
-    tags = a.split("\t")
-    date_col = tags.index("Date")
-    sta_col = tags.index("Station Name")
-    grav_col = tags.index("Gravity")
-    for line in fp:
-        a = line.split("\t")
-        stations.append(a[sta_col])
-stations = [s.upper() for s in stations]        
-stations = list(set(stations))
+    # Get station list and column numbers
+    with open(data_file) as fp:
+        a = fp.readline()
+        tags = a.split("\t")
+        date_col = tags.index("Date")
+        sta_col = tags.index("Station Name")
+        grav_col = tags.index("Gravity")
+        for line in fp:
+            a = line.split("\t")
+            stations.append(a[sta_col])
+    stations = [s.upper() for s in stations]
+    stations = list(set(stations))
 
-# Initialize blank array to hold data. First array of each list element is date, second is gravity.
-data = [[[],[]]]
-for i in range(len(stations)-1):
-    data.append([[],[]])
+    # Initialize blank array to hold data. First array of each list element is date, second is gravity.
+    data = [[[],[]]]
+    for i in range(len(stations)-1):
+        data.append([[],[]])
 
-# Get data from input file
-with open(data_file) as fp:
-    a = fp.readline()
-    for line in fp:
-        a = line.split("\t")
-        sta = a[sta_col].upper()
-        print(sta)
-        sta_index = stations.index(sta)
-        # using the dateutil parser we can plot dates directly
-        data[sta_index][0].append(parser.parse(a[date_col]))
-        data[sta_index][1].append(float(a[grav_col])-offset)
+    # Get data from input file
+    with open(data_file) as fp:
+        a = fp.readline()
+        for line in fp:
+            a = line.split("\t")
+            sta = a[sta_col].upper()
+            print(sta)
+            sta_index = stations.index(sta)
+            # using the dateutil parser we can plot dates directly
+            data[sta_index][0].append(parser.parse(a[date_col]))
+            data[sta_index][1].append(float(a[grav_col])-offset)
 
-offset_data = []
-offset_datum = []
-for d in data:
-    first_datum = d[1][0]
-    out_g = [s - first_datum for s in d[1]]
-    if YAXIS_FT_OF_WATER:
-        out_g = [s/12.77 for s in out_g]
-        ERROR_BAR /= 12.77
-    out_d = [s for s in d[0]]
-    offset_data.append([out_d, out_g])
+    offset_data = []
+    offset_datum = []
+    for d in data:
+        first_datum = d[1][0]
+        out_g = [s - first_datum for s in d[1]]
+        if YAXIS_FT_OF_WATER:
+            out_g = [s/12.77 for s in out_g]
+            ERROR_BAR /= 12.77
+        out_d = [s for s in d[0]]
+        offset_data.append([out_d, out_g])
 
-data = offset_data
+    data = offset_data
 
-#print len(data)
-nfigs = len(data)//4    # floor division: returns integer
-if len(data) % 4 != 0:  # modulo
-    nfigs+=1
+    #print len(data)
+    nfigs = len(data)//4    # floor division: returns integer
+    if len(data) % 4 != 0:  # modulo
+        nfigs+=1
 
-#print nfigs, 4 plots per page
-for i in range(nfigs):
-    plt.figure(figsize=(8.5,11))
-    figidx = i*4
-    for ii in range(4):  
-        if figidx+ii < len(data):
-            if data[figidx+ii][0][0]:
-                plt.subplot(4,1,ii+1)
-                plt.errorbar(data[figidx+ii][0], data[figidx+ii][1], yerr=ERROR_BAR, fmt='kd')
-                ax = plt.gca()
-        # Remove scientific notation from axes labels
-                # ax.yaxis.get_major_formatter().set_useOffset(False)
-        # Add commas to y-axis tick mark labels
-                # ax.yaxis.set_major_formatter(y_format)
-        # Set x-axis tick labels to just show year
-               
+    #print nfigs, 4 plots per page
+    for i in range(nfigs):
+        plt.figure(figsize=(8.5,11))
+        figidx = i*4
+        for ii in range(4):
+            if figidx+ii < len(data):
+                if data[figidx+ii][0][0]:
+                    ax = plt.subplot(4,1,ii+1)
+                    make_plot(ax)
 
-        # Adjust ticks so they fall on Jan 1 and extend past the range of the data. If there
-        # are data in January and December, add another year so that there is plenty of space.
-                if SET_XLIM:
-                    ax.set_xlim([XLEFT, XRIGHT])
-                    #ax.xaxis.set_major_formatter(ALTFMT)
-                else:
-                    ax.xaxis.set_major_formatter(myFmt)
-                    start_month = data[figidx+ii][0][0].month
-                    start_year = data[figidx+ii][0][0].year
-                    end_month = data[figidx+ii][0][-1].month
-                    end_year = data[figidx+ii][0][-1].year
-                    if start_month == 1:
-                        start_year = start_year-1
-                    if end_month == 12:
-                        end_year = end_year + 1
-                    xticks = []
-                    for iii in range(start_year,end_year+2):
-                        xticks.append(datetime.datetime(iii,1,1))
-                    ax.set_xticks(xticks)
-                plt.title(stations[figidx+ii])
-                if YAXIS_LIMITS_TIGHT == 'loose':
-                    max_abs_lim = max(abs(v) for v in ax.get_ylim())
-                    rounded_abs_lim = math.ceil(max_abs_lim)
-                    ax.set_ylim([-1*rounded_abs_lim,rounded_abs_lim])
-                if YAXIS_FT_OF_WATER:
-                    plt.ylabel('Storage change,\nin feet of water')
-                else:
-                    plt.ylabel('Gravity change,\n in microGal')
+                    plt.draw()
+            plt.show()
+            plt.subplots_adjust(bottom=0.25, hspace=0.4, left=0.25, right=0.85)
+    # When saved, this exports fonts as fonts instead of paths:
+            plt.rcParams['svg.fonttype'] = 'none'
+    # This keeps the figure windows open until the user closes them:
+    input()
 
-                plt.draw()
-                
-        plt.subplots_adjust(bottom=0.25, hspace=0.4, left=0.25, right=0.85)
-# When saved, this exports fonts as fonts instead of paths:
-        plt.rcParams['svg.fonttype'] = 'none'
-# This keeps the figure windows open until the user closes them:
-input()
+
